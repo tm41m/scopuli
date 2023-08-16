48527b2bc46b11d856224b6381932fb59fb50659
thecartercodes <thecarter@tm41m.com>
2023-08-16 11:06:05 -0600

    Add price change measure in statcan food prices

--- ganymede/models/stahl/statcan_food_prices.sql
+++ ganymede/models/stahl/statcan_food_prices.sql
@@ -1,16 +1,17 @@
-{{ config(materialized='table', schema='stahl') }}
+{{ config(materialized='table') }}
 
-select
-  ("REF_DATE" || '-01')::date as calender_date
-  , 'monthly' as time_grain
-  , {{ raw_region_to_province('\"GEO\"')}} as region_code
-  , "Products" as r_product_name
-  , lower(substring("Products", '(.*)[,(]')) as product_name
-  , case
+with transform_1 as (
+  select
+    ("REF_DATE" || '-01')::date as calendar_date
+    , 'monthly' as time_grain
+    , {{ raw_region_to_province('\"GEO\"')}} as region_code
+    , "Products" as r_product_name
+    , lower(substring("Products", '(.*)[,(]')) as product_name
+    , case
         when substring("Products", '(\d*\.?\d+)') is not null then substring("Products", '(\d*\.?\d+)')::numeric(32,2) 
         else 1
-    end as amount
-  , case
+      end as amount
+    , case
         when substring(rtrim("Products"), '\s*(\S+)$') = 'grams' then 'g'
         when substring(rtrim("Products"), '\s*(\S+)$') in ('kilogram', 'kilograms') then 'kg'
         when substring(rtrim("Products"), '\s*(\S+)$') in ('millilitres') then 'mL'
@@ -18,7 +19,14 @@ select
         when rtrim(substring(rtrim("Products"), '\s*(\S+)$'), ')') in ('unit', 'bags', 'dozen')
             then rtrim(substring(rtrim("Products"), '\s*(\S+)$'), ')')
         else 'Unknown'
-    end as unit
-  , "VALUE" as price
-  , case when "UOM" = 'Dollars' then 'CAD' else 'Unknown' end as currency
-from {{ source('aethervest', 'statcan_food_prices') }}
+      end as unit
+    , "VALUE" as price
+    , lag("VALUE", 1) over(partition by "GEO", "Products" order by ("REF_DATE" || '-01')::date asc) as previous_price
+    , "DGUID" as r_dguid
+    , case when "UOM" = 'Dollars' then 'CAD' else 'Unknown' end as currency
+  from {{ source('aethervest', 'statcan_food_prices') }}
+)
+select
+  *
+  , (price - previous_price) / previous_price as price_chng 
+from transform_1
