cbb685812a3ff4939fdbc6ec115768ef0395e699
Andrew Shannon <a.shannon9@gmail.com>
2023-10-18 11:23:33 -0400

    removed my old comments from stahl model

--- ganymede/models/stahl/cd_locate_stores.sql
+++ ganymede/models/stahl/cd_locate_stores.sql
@@ -7,20 +7,3 @@ with store_locations as (
 select store_locations.id, cds.cdname, store_locations.region 
 from store_locations inner join {{ source('static_resources', 'censusdivisions') }} cds
 on ST_Covers(cds.geom, ST_Transform(ST_SetSRID(ST_MakePoint(longitude::double precision, latitude::double precision), 4326), 3347))
-
-/*
-select ST_Covers(cds.geom, ST_Transform(ST_SetSRID(ST_MakePoint(longitude::double precision, latitude::double precision), 4326), 3347)) 
-from store_locations, {{ source('static_resources', 'censusdivisions') }} cds
-where cds.pruid='35'
-
-
-join instead of where
-*/
-
-
-/*
-select store_locations.id, cds.cdname 
-from store_locations, {{ source('static_resources', 'censusdivisions') }} cds
-where ST_Covers(cds.geom, ST_Transform(ST_SetSRID(ST_MakePoint(longitude::double precision, latitude::double precision), 4326), 3347))
-*/
-
